ce7cf0b8be2b20b01ad05eeb8bd5cd3b74c6d89b
rudolphattisso <168489419+rudolphattisso@users.noreply.github.com>
2024-06-20 14:38:25 +0200

    Avant le la partie amélioration de la classe

--- src/main/java/fr/afpa/employees/Employee.java
+++ src/main/java/fr/afpa/employees/Employee.java
@@ -17,23 +17,62 @@ class Employee
 	private String registrationNumber;
 
 	// TODO compléter les attributs comme présenté dans le PDF
+	private String lastName;
+	private String firstName;
+	private double salary;
+	private final int socialRate = 30;
 
 	// TODO compléter le constructeur de la classe
-	public Employee(String registrationNumber) {
+	public Employee(String registrationNumber, String lastName, String firstName, double salary) {
 		this.registrationNumber = registrationNumber;
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.salary = salary;
+	
 	}
 
 	// TODO implémenter les setters et getters de la classe (permet d'accéder aux attributs privés)
+//
 	public String getRegistrationNumber() {
 		return registrationNumber;
 	}
+	public String getLastName() {
+			return lastName;
+	}
+	public String getFirstName() {
+				return firstName;
+	}			
+	public double getSalary() {
+				return salary;
+	}
 
-	public void setRegistrationNumber(String registrationNumber) throws Exception {
+
+	public void setRegistrationNumber(String registrationNumber) {
 		this.registrationNumber = registrationNumber;
 	}
 
-	// TODO ajouter les getters et les setters
+	public void setLastname(String lastName)  {
+		this.lastName = lastName;
+	}
+
+	public void setFirstName(String firstName)  {
+		this.firstName = firstName;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+
+
 
 	// TODO implémenter la méthode "toString()" qui renvoie une chaîne de caractère qui représente un objet de la classe employé
 	// plus d'information sur la méthode "toString()" -> https://codegym.cc/fr/groups/posts/fr.986.mthode-java-tostring
+
+	@Override
+	public String toString() {
+		return "Employee [registrationNumber=" + registrationNumber + ", lastName=" + lastName + ", firstName="
+				+ firstName + ", salary=" + salary + ", socialRate=" + socialRate + "]";
+	}
+
 }
--- src/main/java/fr/afpa/employees/EmployeeMain.java
+++ src/main/java/fr/afpa/employees/EmployeeMain.java
@@ -13,8 +13,11 @@ class EmployeeMain
 		System.out.println("\n----- Exercice de programmation objet - classe \"Employee\" -----");
 
 		// TODO instanciez 4 objets de la classe "Employee" (une fois celle-ci implémentée)
-		Employee employee1 = new Employee("11ABC22");
-
+		Employee employee1 = new Employee("jlfj15", "NONO", "Laurent", 1500.0);
+		System.out.println(employee1.getFirstName());
+		employee1.setFirstName("robin");
+		System.out.println(employee1.getFirstName());
+		System.out.println(employee1.toString());
 		// TODO afficher les informations des employés avec System.out.println
 	}
 }
