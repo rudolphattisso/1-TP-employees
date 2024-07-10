648d05addfbf2e7890ac9538561a7b5025441787
Your NameRudolph <attissorudolph@gmail.com>
2024-07-10 11:10:02 +0200

    mise à jour du TP en testant la mise en place d'un logger sur setregistration number

--- src/main/java/fr/afpa/employees/EmployeeMain.java
+++ src/main/java/fr/afpa/employees/EmployeeMain.java
@@ -1,36 +1,40 @@
 package fr.afpa.employees;
 
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Classe principale du projet, contient la fonction "main"
  */
 class EmployeeMain {
+	//rajout d'un attribut privé logger pour jeter afficher un exeption
+	private static Logger logger = LogManager.getLogger(EmployeeMain.class);
 	public static void main(String[] args) {
 		System.out.println("\n----- Exercice de programmation objet - classe \"Employee\" -----");
 		
 		try {
-			// TODO instanciez 4 objets de la classe "Employee" (une fois celle-ci
+			// Tinstanciez 4 objets de la classe "Employee" (une fois celle-ci
 			// implémentée)
 			LocalDate birthDate = LocalDate.parse("1990-02-25");
 			Employee employee1 = new Employee("11AAA33", "Shazam", "Samantha", 1800.0, "1994-12-12");
 			Employee employee2 = new Employee("jlfj16", "NONO", "Zunon", 15075.60, "1995-02-23");
 			Employee employee3 = new Employee("jlfj17", "NONO", "fraqua", 1560.70, "1996-02-24");
-			System.out.println(employee1.getFirstName());
+			logger.trace(employee1.getFirstName());
 			// employee1.setFirstName("robin");
-			System.out.println(employee1.getFirstName());
-			System.out.println(employee1.toString());
-			System.out.println(employee2.netSalary());
+			logger.trace(employee1.getFirstName());
+			logger.trace(employee1.toString());
+			logger.trace(employee2.netSalary());
 
 			// Tentative de modification avec un matricule correctement formaté
 
 			employee1.setRegistrationNumber("11AAA33");
 
-			System.out.println(employee1.getRegistrationNumber());
+			logger.trace(employee1.getRegistrationNumber());
 
-			System.out.println(employee1.toString());
+			logger.trace(employee1.toString());
 			//
 			//String stringConverter = Double.toString(employee1.netSalary());
 
@@ -47,17 +51,17 @@ class EmployeeMain {
 			//implémentatation de la  bouche foreach qui permettra d'imprimer un a un les données des employées
 			//for( type de l'objet - nom de l'objet - nom du tableau)
 			for (Employee emp : employees) {
-			System.out.println(emp.toString());// to string pour traduire les données de sortie en string.
+			logger.trace(emp.toString());// to string pour traduire les données de sortie en string.
 			
 			}
 		} catch (Exception e) {
 
-			System.out.println(e.getMessage());
+			logger.error(e.getMessage());
 		}
 
-		// test des matricules en erreur
+		
 
-		// TODO afficher les informations des employés avec System.out.println
+
 
 	
 
